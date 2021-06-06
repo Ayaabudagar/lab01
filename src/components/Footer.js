e4534c4fa779289ddf75d202c8e6c22362fa76b6
Ayaabudagar <ayakhlil23@yahoo.com>
2021-06-06 19:54:15 +0300

    solve lab 1

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class Footer extends React.Component {
+    render(){
+        return(
+            <div>
+                 <footer>
+        <p>All Right Reserved 2021&copy;Aya</p>
+
+    </footer>
+
+
+            </div>
+        )
+    }
+}
+
+export default Footer;
